5baaafc6ffe3b29b15ca849214a68fd75f0f42a6
ZubSahal <zusah4508@oslomet.no>
2026-03-05 23:10:03 +0100

    Noderegistry

--- rds-backend/src/main/java/com/rds/datastructure/NodeRegistry.java
+++ rds-backend/src/main/java/com/rds/datastructure/NodeRegistry.java
@@ -1,19 +1,18 @@
 package com.rds.datastructure;
-import java.awt.geom.NoninvertibleTransformException;
 import java.util.*;
 
 public class NodeRegistry {
-    private Map<String, List<Node>> nodes = new HashMap<>();  // list of nodes indexed on aspect
-    private List<String> aspects = new ArrayList();  // list of aspects
-    // Hardkode aspekter istedenfor String?
-
-    public Map<String, List<Node>> getNodes() {
+    private Map<String, List<Node>> nodes = new LinkedHashMap<>();  //
+    
+    private List<String> aspects = new ArrayList();  //lists of aspects 
+    //  Hardkode aspekter istedenfor String?
+        return nodes;
+    public Map<String, List<Node>> getNodes() { 
         return nodes;
     }
-
+    
     public List<Node> getNodesByAspect(String aspect){
-
-        // Add aspect to list if not initialized - kan fjernes om aspekter hardkodes
+         // Add aspect to list if not initialized - kan fjernes om aspekter hardkodes
         if (!aspects.contains(aspect)){
             System.out.println("Aspect: " + aspect + " does not exist");  //TODO: improve error handling/logging
             return null;
@@ -21,6 +20,21 @@ public class NodeRegistry {
 
         return nodes.get(aspect);  // returns all sub-nodes of the aspect
     }
+
+    public Node getNodeById(String id){
+        String aspect = id.substring(0, 1); //henter ut aspektet fra første teng i iden. 
+
+        List<Node> nodesInAspect = getNodesByAspect(aspect); //Uses getNodesByAspect to get the right list of nodes
+        //goes through the list
+        for(Node node:NodesInAspect){
+            if(node.getId().equals(id)){
+                return node;  //return node if id matches
+            }
+
+        }
+        return null; //if it is not found
+    }
+
 
     public void addNode(Node node, String aspect) {
         // kansje lage en aspect klasse
