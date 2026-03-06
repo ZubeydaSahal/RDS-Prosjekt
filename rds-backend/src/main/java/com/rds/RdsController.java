eccf9b22b66c471eef96d9da348ebf359db2bebc
koflo7354 <koflo7354@oslomet.no>
2026-03-06 12:43:59 +0100

    fixed parser and ready for testing with controller.

--- rds-backend/src/main/java/com/rds/RdsController.java
+++ rds-backend/src/main/java/com/rds/RdsController.java
@@ -1,17 +1,16 @@
 package com.rds;
 
-import com.rds.datastructure.DataStructure;
-import com.rds.parser.Parser;
+import com.rds.datastructure.*;
+import com.rds.parser.RdsParser;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "")
-
 public class RdsController{
 
     @PostMapping("/parse")
-    public DataStructure parseRds(@RequestBody String rdsScript){
-        Parser parser = new Parser();
-        return parser.parse(rdsScript);
+    public void parseRds(@RequestBody String rdsScript){
+        RdsParser parser = new RdsParser();
+        parser.parse(rdsScript);
     }
 }
