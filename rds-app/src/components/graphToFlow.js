43961785bb80d98cce2359c9ed8c2c206da36b1d
koflo7354 <koflo7354@oslomet.no>
2026-03-13 12:32:56 +0100

    my own version of GraphView.jsx, modified graphToFlow.js to reflect this.

--- rds-app/src/components/graphToFlow.js
+++ rds-app/src/components/graphToFlow.js
@@ -1,15 +1,15 @@
 export function graphToFlow(graph) {
 
     const flowNodes = graph.nodes.map((node) => ({
-        id : node.id,
+        id : node.aspect + ":" + node.id,
         data : {label : node.name},
         position : {x: 0, y: 0}
     }));
 
     const flowRelations = graph.relations.map((rel, index) => ({
         id : "e" + index,
-        source : rel.idForNode1,
-        target : rel.idForNode2,
+        source : rel.aspectForNode1 + ":" + rel.idForNode1,
+        target : rel.aspectForNode2 + ":" + rel.idForNode2,
         label : rel.nameOfRelation
     }));
     return {flowNodes, flowRelations}
