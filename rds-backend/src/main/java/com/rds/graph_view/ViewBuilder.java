315ed5ef8a0000e786a2bbdca10a6112d6b689ce
SeaWil <sewil6031@oslomet.no>
2026-03-27 14:26:24 +0100

    * Added aspect attribute to 'Node' - Refactored all functions to include this aspect
    * Added root definition and configuration
    - setRoot: creates node and sets as root attribute for GraphManager
    - finalizeGraph(): connects root to all top level nodes
    * updated GraphTest to include aspects
    
    ** Works **

---
+++ rds-backend/src/main/java/com/rds/graph_view/ViewBuilder.java
@@ -0,0 +1,10 @@
+package com.rds.graph_view;
+
+import com.rds.datastructure.GraphManager;
+
+public class ViewBuilder {
+    /* Builds a visualization-friendly view of the graph datastructure, without altering the datastructure */
+
+
+
+}
