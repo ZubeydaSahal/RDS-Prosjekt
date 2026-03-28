293b24037e55b7a9857f245b129b6059abd947c7
koflo7354 <koflo7354@oslomet.no>
2026-03-28 11:39:17 +0100

    now added the possibility to import a .txt file to the textbox. does not automatically start visualizing. also added the option to download what is written in the textbox to a txt file.

--- rds-app/src/components/InputPanel.jsx
+++ rds-app/src/components/InputPanel.jsx
@@ -53,6 +53,37 @@ setHasUserInput(true);
     }
   };
 
+  // denne funksjonen lar bruker laste opp en .txt fil programmet og setter det inn i input boksen.
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const text = e.target.result;
+      setText(text);
+    }
+    reader.readAsText(file)
+  }
+
+  // denne funksjonen lagrer alt ifra inputteksten til en .txt fil.
+  const handleDownload = () => {
+    const blob = new Blob([text], {type: "text/plain"});
+
+    const url = URL.createObjectURL(blob)
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "RDSscript.txt";
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url)
+  }
+
   return (
     <div className="input-section">
 
@@ -88,6 +119,12 @@ setHasUserInput(true);
       <div className="buttons">
         <button onClick={handleBuild}>Bygg tre</button>
         <button>Last ned som bilde</button>
+      </div>
+      <div>
+        <input type="file" accept=".txt" onChange={handleFileUpload}/>
+      </div>
+      <div>
+        <button onClick={handleDownload}>download text to file</button>
       </div>
 
     </div>
