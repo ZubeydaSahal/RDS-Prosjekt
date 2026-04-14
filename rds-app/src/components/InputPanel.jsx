fe4b44b7dd9f554d52f3e201a5d1d78a66818ee9
Munaf2 <142993551+Munaf2@users.noreply.github.com>
2026-04-14 18:43:33 +0200

    frontend

--- rds-app/src/components/InputPanel.jsx
+++ rds-app/src/components/InputPanel.jsx
@@ -1,11 +1,16 @@
 function InputPanel({ text, setText, onBuild }) {
 
   const handleKeyDown = (e) => {
+<<<<<<< Updated upstream
     console.log("KEY:", e.key, "META:", e.metaKey, "CTRL:", e.ctrlKey);
   
     if ((e.metaKey || e.ctrlKey) && e.key === "Enter") {
       console.log("CMD+ENTER DETECTED");
       e.preventDefault();
+=======
+    if ((e.metaKey || e.ctrlKey) && e.key === "Enter") {
+      e.preventDefault(); 
+>>>>>>> Stashed changes
       onBuild && onBuild();
     }
   };
@@ -19,7 +24,11 @@ function InputPanel({ text, setText, onBuild }) {
             value={text}
             onChange={(e) => setText(e.target.value)}
             onKeyDown={handleKeyDown}
+<<<<<<< Updated upstream
             placeholder="Paste RDS script here... Press cmd + Enter to build tree"
+=======
+            placeholder="Paste RDS script here..."
+>>>>>>> Stashed changes
         />
       </div>
   );
--- rds-app/src/components/Layout/Navbar.jsx
+++ rds-app/src/components/Layout/Navbar.jsx
@@ -2,10 +2,6 @@ export default function Navbar() {
     return (
       <nav className="navbar">
         <div className="nav-title">RDS Visualizer</div>
-  
-        <div className="nav-links">
-          <a href="#">Documentation</a>
-        </div>
       </nav>
     );
   }
