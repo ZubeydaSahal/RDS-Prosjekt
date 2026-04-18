69ba62b5f78364e0a85194c9ae50f2e39406aa33
ZubeydaSahal <zusah4508@oslomet.no>
2026-04-18 14:34:58 +0200

    farger

--- rds-app/src/components/Node.jsx
+++ rds-app/src/components/Node.jsx
@@ -93,7 +93,7 @@ export default function Node({ node, onToggle, collapsed }) {
 
         fill={
           isRoot
-            ? "#2b448c"
+            ? "#1e3a8a"
             : isAspect
             ? aspectHeaderColors[aspectKey] || "#eee"
             : "#f8f8f8"
@@ -101,7 +101,7 @@ export default function Node({ node, onToggle, collapsed }) {
 
         stroke={
           isRoot
-            ? "#2b448c"
+            ? "#1e3a8a"
             : isAspect
             ? "#bbb"
             : strokeColor
